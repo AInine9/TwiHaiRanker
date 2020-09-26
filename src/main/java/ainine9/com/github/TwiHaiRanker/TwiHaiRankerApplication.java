939a3b00f48446bf6d887c86a5800f9f109943f1
AInine <ainine.jp@gmail.com>
2020-09-27 00:59:23 +0900

    initial commit

---
+++ src/main/java/ainine9/com/github/TwiHaiRanker/TwiHaiRankerApplication.java
@@ -0,0 +1,13 @@
+package ainine9.com.github.TwiHaiRanker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwiHaiRankerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwiHaiRankerApplication.class, args);
+	}
+
+}
