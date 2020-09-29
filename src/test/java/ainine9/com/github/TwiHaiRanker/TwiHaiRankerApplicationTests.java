760892d82e81c823d06c7b495ff0f79afb48ab96
AInine <ainine.jp@gmail.com>
2020-09-29 19:05:31 +0900

    change .gitignore file

---
+++ src/test/java/ainine9/com/github/TwiHaiRanker/TwiHaiRankerApplicationTests.java
@@ -0,0 +1,13 @@
+package ainine9.com.github.TwiHaiRanker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwiHaiRankerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
