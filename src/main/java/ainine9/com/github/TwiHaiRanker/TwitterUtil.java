2cb001c4a1e65406f93c9d747f0c2f5d33d7b2a3
AInine <ainine.jp@gmail.com>
2020-10-03 17:22:41 +0900

    add feature: calculate tweets count per day

--- src/main/java/ainine9/com/github/TwiHaiRanker/TwitterUtil.java
+++ src/main/java/ainine9/com/github/TwiHaiRanker/TwitterUtil.java
@@ -3,21 +3,35 @@ package ainine9.com.github.TwiHaiRanker;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
+import twitter4j.User;
+
+import java.util.Date;
 
 public class TwitterUtil {
     private int count = 0;
+    private Date date = null;
 
     public TwitterUtil(String ID) {
         Twitter twitter = new TwitterFactory().getInstance();
+        User user = null;
 
         try {
-            count = twitter.showUser(ID).getStatusesCount();
+            user = twitter.showUser(ID);
         } catch (TwitterException e) {
-            e.printStackTrace();
+            return;
+        }
+
+        if (user != null) {
+            count = user.getStatusesCount();
+            date = user.getCreatedAt();
         }
     }
 
     public int getCount() {
         return count;
+    }
+
+    public Date getDate() {
+        return date;
     }
 }
--- src/main/java/ainine9/com/github/TwiHaiRanker/controller/MainController.java
+++ src/main/java/ainine9/com/github/TwiHaiRanker/controller/MainController.java
@@ -5,9 +5,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import twitter4j.TwitterException;
+
+import java.math.BigDecimal;
+import java.util.Date;
 
 @Controller
 public class MainController {
+    public static final long oneDateTime = 1000 * 60 * 60 * 24;
+    private String tier, ranking;
 
     @RequestMapping(value = "/main")
     public String main() {
@@ -16,16 +22,34 @@ public class MainController {
 
     @RequestMapping("/check_rank")
     public String checkRank(@RequestParam("ID") String ID, Model model) {
-        String tier, ranking;
-
         TwitterUtil twitterUtil = new TwitterUtil(ID);
-        int count = twitterUtil.getCount();
 
-        tier = String.valueOf(count);
-        ranking = String.valueOf(count);
+        if (twitterUtil.getCount() == 0 || twitterUtil.getDate() == null) {
+            model.addAttribute("tier", "ユーザーが見つからないかツイートをしていません。");
+            model.addAttribute("ranking", "ユーザーが見つからないかツイートをしていません。");
+            return "check_rank";
+        }
+
+        getRank(twitterUtil);
 
         model.addAttribute("tier", tier);
         model.addAttribute("ranking", ranking);
         return "check_rank";
+    }
+
+    public void getRank(TwitterUtil twitterUtil) {
+
+        BigDecimal count = new BigDecimal(twitterUtil.getCount());
+        Date createdDate = twitterUtil.getDate();
+        Date todaysDate = new Date();
+
+        long createdTime = createdDate.getTime();
+        long todaysTime = todaysDate.getTime();
+        BigDecimal diffDays = new BigDecimal((todaysTime - createdTime) / oneDateTime);
+
+        BigDecimal tweetsPerDay = count.divide(diffDays, 2, BigDecimal.ROUND_HALF_UP);
+
+        tier = tweetsPerDay.toPlainString();
+        ranking = tweetsPerDay.toPlainString();
     }
 }
